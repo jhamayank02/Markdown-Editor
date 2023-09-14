cb8d111b7db5f7bc8ad72f437fbbe6b0237eb00d
Mayank Jha <mjha199402@gmail.com>
2023-09-14 11:50:29 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
 import Container from './components/Container';
 import Header from './components/Header';
 import Footer from './components/Footer';
